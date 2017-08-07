7e05fe202974bb3afa3ee6492f13fe07db111633
derek <derek@butterfly.sg>
2017-08-07 12:45:26 +0800

    refactor CoreDataHelper

--- SwiftDevHints/Sources/CoreDataHelper.swift
+++ SwiftDevHints/Sources/CoreDataHelper.swift
@@ -14,3 +14,16 @@ public func create<T: NSManagedObject>(into moc: NSManagedObjectContext) -> T {
     let result = NSEntityDescription.insertNewObject(forEntityName: entityName, into: moc)
     return result as! T
 }
+
+public func createContainer(with name: String, completion: @escaping (NSPersistentContainer) -> ()) {
+    let container = NSPersistentContainer(name: name)
+    container.loadPersistentStores { _, error in
+        guard error == nil else {
+            fatalError("Failed to load container.")
+        }
+        
+        DispatchQueue.main.async {
+            completion(container)
+        }
+    }
+}
