767b4e97751b74015585e7754115b79ed4f0a1f7
derek <derek@butterfly.sg>
2017-08-10 18:57:39 +0800

    md5 for string and string extension for localized function

---
+++ SwiftDevHints/Sources/StringExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  StringExtensions.swift
+//  SwiftDevHints
+//
+//  Created by Julie on 10/8/17.
+//  Copyright © 2017 ZHOU DENGFENG DEREK. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    public var md5: String {
+        return StringProxy(proxy: self).md5
+    }
+}
+
+extension String {
+    public var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
