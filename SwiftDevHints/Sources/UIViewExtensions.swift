23fc5bb2c69ae8582855163fc4da12cf944d661d
Derek <derekcoder@gmail.com>
2018-05-18 11:23:18 +0800

    Add auto layout with key paths

--- SwiftDevHints/Sources/UIViewExtensions.swift
+++ SwiftDevHints/Sources/UIViewExtensions.swift
@@ -40,3 +40,49 @@ extension UIView {
     }
 }
 
+// Auto Layout with Key Paths
+
+public typealias Constraint = (_ child: UIView, _ parent: UIView) -> NSLayoutConstraint
+
+extension UIView {
+    
+    /// Add subview with constraints
+    ///
+    /// Referenced from https://talk.objc.io/episodes/S01E75-auto-layout-with-key-paths
+    ///
+    ///     parentView.addSubview(subview, constraints: [
+    ///         equal(\.topAnchor),
+    ///         equal(\.leadingAnchor),
+    ///         equal(\.trailingAnchor),
+    ///         equal(\.heightAnchor, to: 200),
+    ///     ])
+    ///
+    /// - Parameters:
+    ///   - other: subview
+    ///   - constraints: constraints to added
+    public func addSubview(_ other: UIView, constraints: [Constraint]) {
+        addSubview(other)
+        other.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(constraints.map { c in
+            c(other, self)
+        })
+    }
+}
+
+public func equal<L>(_ keyPath: KeyPath<UIView, L>, to constant: CGFloat) -> Constraint where L: NSLayoutDimension {
+    return { view, parent in
+        view[keyPath: keyPath].constraint(equalToConstant: constant)
+    }
+}
+
+public func equal<Axis, L>(_ keyPath: KeyPath<UIView, L>) -> Constraint where L: NSLayoutAnchor<Axis> {
+    return { view, parent in
+        view[keyPath: keyPath].constraint(equalTo: parent[keyPath: keyPath])
+    }
+}
+
+public func equal<Axis, L>(_ from: KeyPath<UIView, L>, _ to: KeyPath<UIView, L>) -> Constraint where L: NSLayoutAnchor<Axis> {
+    return { view, parent in
+        view[keyPath: from].constraint(equalTo: parent[keyPath: to])
+    }
+}
