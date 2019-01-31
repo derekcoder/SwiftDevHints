e32bf692e3aba986c556eb5cd16edb91ef77c199
DerekCoder <derekcoder@gmail.com>
2019-01-31 14:37:20 +0800

    Add isBackground & isForeground for UIApplication

---
+++ SwiftDevHints/Sources/UIApplicationExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UIApplicationExtension.swift
+//  SwiftDevHints
+//
+//  Created by Derek on 31/1/19.
+//  Copyright © 2019 ZHOU DENGFENG DEREK. All rights reserved.
+//
+
+import UIKit
+
+public extension UIApplication {
+    var isBackground: Bool {
+        return !isForeground
+    }
+    
+    var isForeground: Bool {
+        return applicationState == .active
+    }
+}
