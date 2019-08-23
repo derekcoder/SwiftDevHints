16cabbb566c128a910d9b7dc4ba47add0c70e56d
derekcoder <derekcoder@gmail.com>
2019-08-23 20:29:32 +0800

    Add NibReusable for UITableViewHeaderFooterView

--- SwiftDevHints/Sources/UITableView+NibReusable.swift
+++ SwiftDevHints/Sources/UITableView+NibReusable.swift
@@ -27,3 +27,20 @@ public extension UITableView {
         return cell
     }
 }
+
+public extension UITableView {
+    final func register<T: UITableViewHeaderFooterView>(_ cellType: T.Type) where T: NibReusable {
+        register(cellType.nib, forHeaderFooterViewReuseIdentifier: cellType.reuseIdentifier)
+    }
+    
+    final func register<T: UITableViewHeaderFooterView>(_ cellType: T.Type) where T: Reusable {
+        register(cellType.self, forHeaderFooterViewReuseIdentifier: cellType.reuseIdentifier)
+    }
+    
+    final func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>(cellType: T.Type = T.self) -> T where T: Reusable {
+        guard let headerFooterView = dequeueReusableHeaderFooterView(withIdentifier: cellType.reuseIdentifier) as? T else {
+            fatalError("The \(String(describing: T.self)) couldn't be dequeued.")
+        }
+        return headerFooterView
+    }
+}
